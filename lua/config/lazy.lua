ccdd7b36a5dcd585d4dcf33089f0523b038feefc
Ihar Katkavets <job4ihar@gmail.com>
2024-09-21 11:27:43 +0200

    master Update config structure

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -22,43 +22,11 @@ vim.opt.rtp:prepend(lazypath)
 --
 -- NOTE: Here is where you install your plugins.
 require('lazy').setup({
-  -- The following two comments only work if you have downloaded the kickstart repo, not just copy pasted the
-  -- init.lua. If you want these files, they are in the repository, so you can just download them and
-  -- place them in the correct locations.
-
-  -- NOTE: Next step on your Neovim journey: Add/Configure additional plugins for Kickstart
-  --
-  --  Here are some example plugins that I've included in the Kickstart repository.
-  --  Uncomment any of the lines below to enable them (you will need to restart nvim).
-  --
-  --require 'kickstart.plugins.debug',
+  -- require 'kickstart.plugins.debug',
   -- require 'plugins.indent_line',
-  require 'plugins.colorschemes',
-  require 'plugins.lint',
-  require 'plugins.autopairs',
-  require 'plugins.neo-tree',
-  require 'plugins.gitsigns',
   -- require 'plugins.stackmap',
-  require 'plugins.comment',
-  require 'plugins.codeforces-templates',
-  require 'plugins.vim-sleuth',
-  require 'plugins.which-key',
-  require 'plugins.telescope',
-  require 'plugins.lazydev',
-  require 'plugins.luvit-meta',
-  require 'plugins.nvim-lspconfig',
-  require 'plugins.conform',
-  require 'plugins.nvim-cmp',
-  require 'plugins.todo-comments',
-  require 'plugins.mini',
-  require 'plugins.nvim-treesitter',
 
-  -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
-  --    This is the easiest way to modularize your config.
-  --
-  --  Uncomment the following line and add your plugins to `lua/custom/plugins/*.lua` to get going.
-  --    For additional information, see `:help lazy.nvim-lazy.nvim-structuring-your-plugins`
-  -- { import = 'custom.plugins' },
+  { import = 'plugins.always' },
 }, {
   concurrency = 10,
   ui = {
